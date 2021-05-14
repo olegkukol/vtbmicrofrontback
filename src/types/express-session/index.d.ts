55adb6071637079e9e06a34a63442252d7e3713e
Evgeniy Akishin <eakishin@luxoft.com>
2021-05-14 17:38:07 +0300

    fix: express-session type

--- src/types/express-session/index.d.ts
+++ src/types/express-session/index.d.ts
@@ -4,5 +4,12 @@ declare module 'express-session' {
   export interface Session {
     userId: string;
     isLogged: boolean;
+    user: {
+      teamId: string;
+      streamId: string;
+      role: 'HEAD_OF_DEPARTMENT' | 'HEAD_OF_STREAM' | 'HEAD_OF_TEAM' | 'EMPLOYEE';
+      type: 'INTERNAL' | 'OUTSTAFF';
+      id: string;
+    };
   }
 }
